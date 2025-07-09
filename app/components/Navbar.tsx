6fcc1519b219c0a6b7317ad43d5ef9b7aed8ef69
amadrafi <ahmadrafi0814@gmail.com>
2025-07-10 00:32:04 +0100

    finished the descriptions

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
         <div className="flex flex-row justify-between items-center h-16">
             <div className="flex items-center gap-2">
                 <h1 className={`text-xl font-semibold ${poppins.className} hidden lg:block md:block`} style={{ color: '#15494A'}}>Bracket AI</h1>
-                <Image src="/logo.png" alt="Bracket AI" width={50} height={50} />
+                <Image src="/logo.PNG" alt="Bracket AI" width={50} height={50} />
             </div>
             <div className={`flex flex-row items-center font-medium gap-3 md:gap-5 lg:gap-5 ${poppins.className}`} style={{ color: "#15494A"}}>
                 <h1>About</h1>
