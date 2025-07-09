8f528d5616a3f46eb456b0847a552c6e7ddb7c87
amadrafi <ahmadrafi0814@gmail.com>
2025-07-10 00:33:17 +0100

    finished the descriptions

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
               <div className="px-4 md:px-10 flex flex-col gap-4 justify-center items-start">
                 <p className="text-2xl md:text-3xl font-bold">A Solution That Works</p>
                 <p className="text-lg md:text-xl">Blending established psychological insight and machine learning to create a <span className="font-bold text-2xl">smarter</span>, more <span className="font-bold text-2xl">empathetic</span> approach to compatibility.</p>
-                <p className="text-xl md:text-2xl lg:text-3xl font-medium">…with our model achieving <span className="font-bold text-3xl md:text-4xl">83.9%</span> accuracy and <span className="font-bold text-3xl md:text-4xl">improving every day!</span></p>
+                <p className="text-xl md:text-2xl lg:text-3xl font-medium">…with our model already achieving <span className="font-bold text-3xl md:text-4xl">83.9%</span> accuracy and <span className="font-bold text-3xl md:text-4xl">improving every day!</span></p>
               </div>
             </div>
             <div className='w-full md:w-1/2 aspect-square md:aspect-auto md:h-96 lg:h-110 rounded-3xl overflow-hidden order-1 md:order-2 lg:order-2'>
