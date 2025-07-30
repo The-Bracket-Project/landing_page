e2a5122e72703481c8617012aa708102a98d640d
amadrafi <ahmadrafi0814@gmail.com>
2025-07-30 21:59:09 +0100

    added dashboard again

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -92,7 +92,7 @@ function Navbar() {
               <h1>Contact</h1>
             </Link>
             <Link
-              href="/contactus"
+              href="https://dashboard.thebracket.io/"
               className="hover:opacity-70 transition-all duration-200 hover:transform hover:scale-105"
             >
               <h1>Dashboard</h1>
